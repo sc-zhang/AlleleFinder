44493afea2e2aa80df64cba82a3005cb97c71052
sc-zhang <765550282@qq.com>
2020-11-30 16:15:39 +0800

    add module

---
+++ bin/format_allele_table.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+import sys
+import re
+
+
+def format_allel_table(in_allele, mono_gff3, hap_gff3, blast, tandem, allele_num, out_allele):
+	print("Loading blast")
+	blast_db = {}
+	with open(blast, 'r') as fin:
+		for line in fin:
+			data = line.strip().split()
+			hap_gn = data[0]
+			mono_gn = data[1]
+			bs = float(data[-1])
+			if hap_gn not in blast_db:
+				blast_db[hap_gn] = [mono_gn, bs]
+			if bs > blast_db[hap_gn][-1]:
+				blast_db[hap_gn] = [mono_gn, bs]
+	
+	print("Loading mono gff3")
+	mono_db = {}
+	with open(mono_gff3, 'r') as fin:
+		for line in fin:
+			if line.strip() == '' or line[0] == '#':
+				continue
+			data = line.strip().split()
+			if data[2] == 'gene':
+				id = data[8].split(";")[0].split('=')[1]
+				if id not in mono_db:
+					mono_db[id] = [data[0], int(data[3])]
+	
+	print("Loading hap_gff3")
+	hap_db = {}
+	with open(hap_gff3, 'r') as fin:
+		for line in fin:
+			if line.strip() == '' or line[0] == '#':
+				continue
+			data = line.strip().split()
+			if data[2] != 'gene':
+				continue
+			chrn = data[0]
+			id = data[8].split(";")[0].split('=')[1]
+			hap_db[id] = [chrn, int(data[3])]
+	
+	print("Loading tandem")
+	tandem_db = {}
+	with open(tandem, 'r') as fin:
+		for line in fin:
+			data = line.strip().split(',')
+			for id in data:
+				tandem_db[id] = 1
+	
+	print("Formatting allele table")
+	full_allele = []
+	info_db = {}
+	with open(in_allele, 'r') as fin:
+		for line in fin:
+			data = line.strip().split(',')
+			tmp_db = {}
+			for id in data:
+				hchrn, hpos = hap_db[id]
+				hidx = ord(hchrn[-1])-65
+				if id not in blast_db:
+					continue
+				mono_gn = blast_db[id][0]
+				if mono_gn not in mono_db:
+					continue
+				if mono_gn not in info_db:
+					info_db[mono_gn] = [[] for i in range(0, allele_num)]
+				info_db[mono_gn][hidx].append([hpos, id])
+		
+		for mono_gn in info_db:
+			tmp_list = []
+			null_cnt = 0
+			for i in range(0, len(info_db[mono_gn])):
+				if info_db[mono_gn][i] == []:
+					info_db[mono_gn][i] = "NA"
+					null_cnt += 1
+				else:
+					tmp_list = sorted(info_db[mono_gn][i])
+					tmp_ids = []
+					tmp_ids.append(tmp_list[0][-1])
+					if len(tmp_list) > 1:
+						for j in range(1, len(tmp_list)):
+							pos, id = tmp_list[j]
+							if id in tandem_db:
+								tmp_ids.append(id+"-T")
+							else:
+								tmp_ids.append(id+"-P")
+					info_db[mono_gn][i] = ','.join(tmp_ids)
+
+			if null_cnt == allele_num:
+				continue
+			
+			tmp_list = mono_db[mono_gn]
+			tmp_list.append(mono_gn)
+			tmp_list.extend(info_db[mono_gn])
+			full_allele.append(tmp_list)
+			
+	
+	print("Writing allele table")
+	with open(out_allele, 'w') as fout:
+		with open(out_allele+'.simple', 'w') as fsimple:
+			allele_header = []
+			for i in range(0, allele_num):
+				allele_header.append("Allele %s"%(chr(i+65)))
+			fout.write("#CHR\tPOS\tMono gene\t%s\n"%('\t'.join(allele_header)))
+			cnt = 0
+			for info in sorted(full_allele):
+				fout.write("%s\n"%('\t'.join(map(str, info))))
+				tmp_list = []
+				for id in info[3:]:
+					if id != "NA":
+						tmp_list.append(id)
+				fsimple.write("%s\n"%(','.join(tmp_list).replace('-T', '').replace('-P', '')))
+	print("Finished")
+
+
+if __name__ == "__main__":
+	if len(sys.argv) < 8:
+		print("Usage: python %s <in_allele> <mono_gff3> <hap_gff3> <hap_mono_blast> <tandem_list> <allele_number> <out_allele>"%sys.argv[0])
+	else:
+		in_allele, mono_gff3, hap_gff3, blast, tandem, allele_num, out_allele = sys.argv[1:]
+		format_allel_table(in_allele, mono_gff3, hap_gff3, blast, tandem, int(allele_num), out_allele)
---
+++ bin/stat_formated_allele.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+import sys
+import re
+
+
+def stat_formated_allele(in_allele, in_hap_gff3, out_stat):
+	print("Loading hap gff3")
+	hap_db = {}
+	with open(in_hap_gff3, 'r') as fin:
+		for line in fin:
+			if line.strip() == '' or line[0] == '#':
+				continue
+			data = line.strip().split()
+			if 'ctg' in data[0] or 'tig' in data[0] or 'utg' in data[0]:
+				continue
+			chrn = "Chr%02d"%(int(re.findall(r'[A-Za-z](\d+)', data[0])[0]))
+			if "Name" in data[8]:
+				id = re.findall(r'Name=(.*)', data[8])[0].split(';')[0]
+			else:
+				id = re.findall(r'ID=(.*)', data[8])[0].split(';')[0]
+			hap_db[id] = chrn
+	
+	print("Loading allele table")
+	allele_summary = {}
+	with open(in_allele, 'r') as fin:
+		for line in fin:
+			data = line.strip().split('\t')
+			if line[0] == '#':
+				allele_cnt = len(data)-3
+				continue
+
+			tand_cnt = 0
+			disp_cnt = 0
+			allele_cnt_list = [0 for i in range(0, allele_cnt)]
+			for i in range(3, len(data)):
+				if data[i] == 'NA':
+					continue
+				ids = data[i].split(',')
+				chrn_cnt_db = {}
+				allele_cnt_list[i-3] += 1
+				for id in ids:
+					if '-' in id:
+						id, type = id.split('-')
+						if type == 'T':
+							tand_cnt += 1
+						else:
+							disp_cnt += 1
+					if id in hap_db:
+						chrn = hap_db[id]
+						if chrn not in chrn_cnt_db:
+							chrn_cnt_db[chrn] = 0
+						chrn_cnt_db[chrn] += 1
+			max_chrn = ""
+			max_cnt = 0
+			for chrn in chrn_cnt_db:
+				if chrn_cnt_db[chrn] > max_cnt:
+					max_cnt = chrn_cnt_db[chrn]
+					max_chrn = chrn
+			if max_chrn not in allele_summary:
+				allele_summary[max_chrn] = [[0 for i in range(0, allele_cnt)], 0, 0]
+			
+			for i in range(0, allele_cnt):
+				allele_summary[max_chrn][0][i] += allele_cnt_list[i]
+			
+			allele_summary[max_chrn][1] += tand_cnt
+			allele_summary[max_chrn][2] += disp_cnt
+	
+	print("Writing summary")
+	with open(out_stat, 'w') as fout:
+		allele_list = []
+		for i in range(0, allele_cnt):
+			allele_list.append("Allele %s"%chr(65+i))
+		fout.write("#CHR,%s,Tandem,Dispersely\n"%(','.join(allele_list)))
+		sum_info = [0 for i in range(0, allele_cnt+2)]
+		for chrn in sorted(allele_summary):
+			info = [chrn]
+			info.extend(allele_summary[chrn][0])
+			info.append(allele_summary[chrn][1])
+			info.append(allele_summary[chrn][2])
+			for i in range(0, len(allele_summary[chrn][0])):
+				sum_info[i] += allele_summary[chrn][0][i]
+			sum_info[allele_cnt] += allele_summary[chrn][1]
+			sum_info[allele_cnt+1] += allele_summary[chrn][2]
+			fout.write("%s\n"%(','.join(map(str, info))))
+		fout.write("Total,%s\n"%(','.join(map(str, sum_info))))
+	print("Finished")
+
+
+if __name__ == "__main__":
+	if len(sys.argv) < 4:
+		print("Usage: python %s <in_allele> <in_hap_gff3> <out_stat>"%sys.argv[0])
+	else:
+		in_allele, in_hap_gff3, out_stat = sys.argv[1:]
+		stat_formated_allele(in_allele, in_hap_gff3, out_stat)
