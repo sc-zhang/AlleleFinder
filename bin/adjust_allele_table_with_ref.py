ace1574135e038c5b38047646a9641552650f67a
jiaxin Yu <52374093+Yujiaxin419@users.noreply.github.com>
2023-02-19 08:30:45 +0800

    Update adjust_allele_table_with_ref.py

--- bin/adjust_allele_table_with_ref.py
+++ bin/adjust_allele_table_with_ref.py
@@ -2,9 +2,9 @@
 import sys
 import re
 import copy
+import dup_pipe as dp
 
-
-def adjust_allele_table(in_allele, ref_gff3, hap_gff3, ref_blast, hap_blast, iden, tandem, allele_num, out_allele, is_mono):
+def adjust_allele_table(in_allele, ref_gff3, hap_gff3, ref_blast, hap_blast, iden, tandem, allele_num, out_allele, is_mono, pFile, tFile):
 	print("Loading ref blast")
 	ref_blast_db = {}
 	with open(ref_blast, 'r') as fin:
@@ -87,11 +87,11 @@ def adjust_allele_table(in_allele, ref_gff3, hap_gff3, ref_blast, hap_blast, ide
 				hidx = ord(hchrn[-1])-65
 				if id not in ref_blast_db:
 					if id not in hap_blast_db:
-						continue
+						continue  # 这种一般是singleton基因
 					ref_id = hap_blast_db[id][0]
 				else:
 					ref_id = id
-				if ref_id not in ref_blast_db or ref_id not in data:
+				if ref_id not in ref_blast_db or ref_id not in data:  # id不在等位中或者比对不上reference
 					continue
 				ref_gn = ref_blast_db[ref_id][0]
 				if ref_gn not in ref_db:
@@ -105,7 +105,9 @@ def adjust_allele_table(in_allele, ref_gff3, hap_gff3, ref_blast, hap_blast, ide
 					if tmp_gn not in info_db:
 						info_db[tmp_gn] = [[] for i in range(0, allele_num)]
 					info_db[tmp_gn][hidx].append([hpos, id])
-			
+		
+		dup_gene_db = {}
+		allele2ref = {}
 		for ref_gn in info_db:
 			tmp_list = []
 			null_cnt = 0
@@ -120,10 +122,14 @@ def adjust_allele_table(in_allele, ref_gff3, hap_gff3, ref_blast, hap_blast, ide
 					if len(tmp_list) > 1:
 						for j in range(1, len(tmp_list)):
 							pos, id = tmp_list[j]
-							if id in tandem_db:
-								tmp_ids.append(id+"-T")
-							else:
-								tmp_ids.append(id+"-P")
+							dup_gene_db[id]
+						tmp_ids = tmp_list[0][-1]
+						
+#							if id in tandem_db:
+#								tmp_ids.append(id+"-T")
+#							else:
+#								tmp_ids.append(id+"-P")
+					#allele2ref[tmp_ids] = ref_g
 					info_db[ref_gn][i] = ','.join(tmp_ids)
 
 			if null_cnt == allele_num:
@@ -135,8 +141,113 @@ def adjust_allele_table(in_allele, ref_gff3, hap_gff3, ref_blast, hap_blast, ide
 			tmp_list = copy.deepcopy(ref_db[ori_gn])
 			tmp_list.append(ori_gn)
 			tmp_list.extend(info_db[ref_gn])
+			for tmp_g in info_db[ref_gn]:
+				allele2ref[tmp_g] = ref_gn
 			full_allele.append(tmp_list)
-		
+	#### 开始重新分配被拉取下来的基因	
+		singleton = {}
+		allele2table = {}
+		addition_allele = {}
+		dup_classter = {}
+		network2gn = {}
+		gn2network = {}
+		nc = 0
+		for g in dup_gene_db:
+			if g not in hap_blast_db:
+				singleton[g] = ''
+				continue
+			else:
+				blast_gn = hap_blast_db[g][0]
+				if blast_gn in dup_gene_db:
+					if blast_gn not in gn2network and g not in gn2network:
+						nc += 1
+						network2gn[nc] = set([blast_gn, g])
+						gn2network[blast_gn] = nc
+						gn2network[g] = nc
+					elif g in gn2network:
+						nc = gn2network[g]
+						network2gn[nc].add(blast_gn)
+					else:
+						nc = gn2network[blast_gn]
+						network2gn[nc].add(g)
+				elif blast_gn in allele2ref:
+					ref_gn = allele2ref[blast_gn]
+					hchrn, hpos = hap_db[id]
+					hidx = ord(hchrn[-1])-65
+					candidate_gn = info_db[ref_gn][hidx]
+					dup_classter[g] = [ref_gn, 'remove']
+					if candidate_gn == 'NA':
+						info_db[ref_gn][hidx] = g
+						dup_classter[g][1] = 'keep'
+				else:
+					continue
+		gnNet = list(network2gn.values())
+		for net in gnNet:
+			# check if g in allele table
+			tmp_ref_gn = 'NA'
+			for tmpg in net:
+				if tmpg in dup_classter:
+					tmp_ref_gn = dup_classter[g][0]
+			if tmp_ref_gn == 'NA':
+				ref_gn = list(net)[0]
+				info_db[ref_gn] = ['NA' for i in range(0, allele_num)]
+				for tmpg in net:
+					hchrn, hpos = hap_db[tmpg]
+					hidx = ord(hchrn[-1])-65
+					info_db[ref_gn][hidx] = tmpg
+				addition_allele[ref_gn] = ''
+			else:
+				tmpAddition = []
+				for tmpg in net: # check every gene
+					hchrn, hpos = hap_db[tmpg]
+					hidx = ord(hchrn[-1])-65
+					candidate_gn = info_db[tmp_ref_gn][hidx]
+					# 其中几个基因能被拉回等位表中
+					if candidate_gn == 'NA':
+						info_db[tmp_ref_gn][hidx] = tmpg
+						if tmp_ref_gn not in allele2table:
+							allele2table[tmp_ref_gn] = ''
+					elif dup_classter[tmpg][1] == 'keep':
+						# 避免能挂到等位表上的基因被去掉
+						if tmp_ref_gn not in allele2table:
+							allele2table[tmp_ref_gn] = ''
+					else:
+						tmpAddition.append(tmpg)
+				if len(tmpAddition) >= 1:
+					ref_gn = tmpAddition[0]
+					info_db[ref_gn] = ['NA' for i in range(0, allele_num)]
+				for tmpg in tmpAddition:
+					hchrn, hpos = hap_db[tmpg]
+					hidx = ord(hchrn[-1])-65
+					info_db[ref_gn][hidx] = tmpg
+				addition_allele[ref_gn] = ''
+				# 将挂不上等位表的基因自成一行
+			
+		for dup_in_allele in dup_classter:
+			ref_gn = dup_classter[dup_in_allele][0]
+			if ref_gn not in allele2table and dup_in_allele not in gn2network:
+				hchrn, hpos = hap_db[dup_in_allele]
+				hidx = ord(hchrn[-1])-65
+				info_db[ref_gn] = ['NA' for i in range(0, allele_num)]
+				candidate_gn = info_db[dup_in_allele][hidx]
+				if candidate_gn == 'NA':
+					info_db[ref_gn][hidx] = dup_in_allele
+					allele2table[ref_gn] = ''
+				else:
+					ref_gn = dup_in_allele
+					info_db[ref_gn][hidx] = dup_in_allele
+					addition_allele[ref_gn] = ''
+		for sg in singleton:
+			ref_gn = sg
+			hchrn, hpos = hap_db[sg]
+			hidx = ord(hchrn[-1])-65
+			info_db[ref_gn] = ['NA' for i in range(0, allele_num)]
+			info_db[ref_gn][hidx] = sg
+	### 重新加上paralog的基因
+	### load pDic & tDic
+	pDic = dp.read_repeat_file(pFile)
+	tDic = dp.read_repeat_file(tFile)
+	full_allele = dp.add_dup_info(full_allele, info_db, hap_db, singleton, allele2table, addition_allele, pDic, tDic)
 	print("Writing allele table")
 	with open(out_allele, 'w') as fout:
 		allele_header = []
@@ -152,7 +263,7 @@ def adjust_allele_table(in_allele, ref_gff3, hap_gff3, ref_blast, hap_blast, ide
 
 if __name__ == "__main__":
 	if len(sys.argv) < 11:
-		print("Usage: python %s <in_allele> <ref_gff3> <hap_gff3> <hap_ref_blast> <hap_hap_blast> <iden_threshold> <tandem_list> <allele_number> <out_allele> <is_mono>"%sys.argv[0])
+		print("Usage: python %s <in_allele> <ref_gff3> <hap_gff3> <hap_ref_blast> <hap_hap_blast> <iden_threshold> <tandem_list> <allele_number> <out_allele> <is_mono> <pFile> <tFile>"%sys.argv[0])
 	else:
-		in_allele, ref_gff3, hap_gff3, ref_blast, hap_blast, iden, tandem, allele_num, out_allele, is_mono = sys.argv[1:]
-		adjust_allele_table(in_allele, ref_gff3, hap_gff3, ref_blast, hap_blast, float(iden), tandem, int(allele_num), out_allele, is_mono)
+		in_allele, ref_gff3, hap_gff3, ref_blast, hap_blast, iden, tandem, allele_num, out_allele, is_mono, pFile, tFile = sys.argv[1:]
+		adjust_allele_table(in_allele, ref_gff3, hap_gff3, ref_blast, hap_blast, float(iden), tandem, int(allele_num), out_allele, is_mono, pFile, tFile)
