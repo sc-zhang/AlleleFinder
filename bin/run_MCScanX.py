f48bd38dffd57940f95430a88ac29822b23431da
sc-zhang <zsc-zhang@foxmail.com>
2024-09-18 15:35:47 +0800

    record external tools logs

--- bin/run_MCScanX.py
+++ bin/run_MCScanX.py
@@ -5,87 +5,89 @@ import re
 
 
 def check_fasta_type(in_fa):
-	with open(in_fa, 'r') as fin:
-		for line in fin:
-			if line[0] == ">":
-				continue
-			if "M" in line.upper():
-				return "PROT"
-			else:
-				return "NUCL"
+    with open(in_fa, 'r') as fin:
+        for line in fin:
+            if line[0] == ">":
+                continue
+            if "M" in line.upper():
+                return "PROT"
+            else:
+                return "NUCL"
 
 
 def run_MCScanX(in_fa, in_gff3, out_dir, threads):
-	in_fa = os.path.abspath(in_fa)
-	in_gff3 = os.path.abspath(in_gff3)
-	if not os.path.exists(out_dir):
-		os.mkdir(out_dir)
-	
-	print("Entering %s"%out_dir)
-	os.chdir(out_dir)
+    in_fa = os.path.abspath(in_fa)
+    in_gff3 = os.path.abspath(in_gff3)
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
 
-	if not os.path.exists("xyz"):
-		os.mkdir("xyz")
-	
-	if not os.path.exists("xyz/xyz.blast"):
-		print("Running blast")
-		fa_type = check_fasta_type(in_fa)
-		if fa_type == "NUCL":
-			cmd1 = "makeblastdb -in %s -dbtype nucl -out blastdb &> /dev/null"%in_fa
-			cmd2 = "blastn -query %s -db blastdb -out xyz/xyz.blast -evalue 1e-10 -outfmt 6 -num_alignments 5 -num_threads %d &> /dev/null"%(in_fa, threads)
-		else:
-			cmd1 = "makeblastdb -in %s -dbtype prot -out blastdb &> /dev/null"%in_fa
-			cmd2 = "blastp -query %s -db blastdb -out xyz/xyz.blast -evalue 1e-10 -outfmt 6 -num_alignments 5 -num_threads %d &> /dev/null"%(in_fa, threads)
-		
-		print("Running command: %s"%cmd1)
-		os.system(cmd1)
-		print("Running command: %s"%cmd2)
-		os.system(cmd2)
-	else:
-		print("Blast file found, skip")
+    print("Entering %s" % out_dir)
+    os.chdir(out_dir)
 
-	if not os.path.exists("xyz/xyz.gff"):
-		print("Loading gff3")
-		gff3_db = {}
-		with open(in_gff3, 'r') as fin:
-			for line in fin:
-				if line.strip() == '' or line[0] == '#':
-					continue
-				data = line.strip().split()
-				chrn = re.findall(r'([A-Za-z]+\d+)', data[0])[0]
-				if 'tig' in chrn or 'ctg' in chrn:
-					continue
-				if data[2] == 'gene':
-					if "Name" in data[8]:
-						id = re.findall(r'Name=(.*)', data[8])[0].split(';')[0]
-					else:
-						id = re.findall(r'ID=(.*)', data[8])[0].split(';')[0]
-					
-					if chrn not in gff3_db:
-						gff3_db[chrn] = []
-					gff3_db[chrn].append([id, int(data[3]), int(data[4])])
-		
-		print("Writing gff")
-		idx = 1
-		with open(os.path.join("xyz/xyz.gff"), 'w') as fout:
-			for chrn in sorted(gff3_db):
-				spid = "NN%02d"%(idx)
-				idx += 1
-				for id, sp, ep in sorted(gff3_db[chrn]):
-					fout.write("%s\t%s\t%d\t%d\n"%(spid, id, sp, ep))
-	else:
-		print("GFF file found, skip")
-	
-	cmd = "MCScanX xyz/xyz &> /dev/null"
-	print("Running command: %s"%cmd)
-	os.system(cmd)
+    if not os.path.exists("xyz"):
+        os.mkdir("xyz")
 
-	print("Finished")	
+    if not os.path.exists("xyz/xyz.blast"):
+        print("Running blast")
+        fa_type = check_fasta_type(in_fa)
+        if fa_type == "NUCL":
+            cmd1 = "makeblastdb -in %s -dbtype nucl -out blastdb &> blast.log" % in_fa
+            cmd2 = ("blastn -query %s -db blastdb -out xyz/xyz.blast -evalue 1e-10 -outfmt 6 -num_alignments 5 "
+                    "-num_threads %d &>> blast.log") % (in_fa, threads)
+        else:
+            cmd1 = "makeblastdb -in %s -dbtype prot -out blastdb &> blast.log" % in_fa
+            cmd2 = ("blastp -query %s -db blastdb -out xyz/xyz.blast -evalue 1e-10 -outfmt 6 -num_alignments 5 "
+                    "-num_threads %d &>> blast.log") % (in_fa, threads)
+
+        print("Running command: %s" % cmd1)
+        os.system(cmd1)
+        print("Running command: %s" % cmd2)
+        os.system(cmd2)
+    else:
+        print("Blast file found, skip")
+
+    if not os.path.exists("xyz/xyz.gff"):
+        print("Loading gff3")
+        gff3_db = {}
+        with open(in_gff3, 'r') as fin:
+            for line in fin:
+                if line.strip() == '' or line[0] == '#':
+                    continue
+                data = line.strip().split()
+                chrn = re.findall(r'([A-Za-z]+\d+)', data[0])[0]
+                if 'tig' in chrn or 'ctg' in chrn:
+                    continue
+                if data[2] == 'gene':
+                    if "Name" in data[8]:
+                        id = re.findall(r'Name=(.*)', data[8])[0].split(';')[0]
+                    else:
+                        id = re.findall(r'ID=(.*)', data[8])[0].split(';')[0]
+
+                    if chrn not in gff3_db:
+                        gff3_db[chrn] = []
+                    gff3_db[chrn].append([id, int(data[3]), int(data[4])])
+
+        print("Writing gff")
+        idx = 1
+        with open(os.path.join("xyz/xyz.gff"), 'w') as fout:
+            for chrn in sorted(gff3_db):
+                spid = "NN%02d" % (idx)
+                idx += 1
+                for id, sp, ep in sorted(gff3_db[chrn]):
+                    fout.write("%s\t%s\t%d\t%d\n" % (spid, id, sp, ep))
+    else:
+        print("GFF file found, skip")
+
+    cmd = "MCScanX xyz/xyz &> mcscanx.log"
+    print("Running command: %s" % cmd)
+    os.system(cmd)
+
+    print("Finished")
 
 
 if __name__ == "__main__":
-	if len(sys.argv) < 5:
-		print("Usage: python %s <in_fasta> <in_gff3> <out_dir> <threads>"%sys.argv[0])
-	else:
-		in_fa, in_gff3, out_dir, threads = sys.argv[1:]
-		run_MCScanX(in_fa, in_gff3, out_dir, int(threads))
+    if len(sys.argv) < 5:
+        print("Usage: python %s <in_fasta> <in_gff3> <out_dir> <threads>" % sys.argv[0])
+    else:
+        in_fa, in_gff3, out_dir, threads = sys.argv[1:]
+        run_MCScanX(in_fa, in_gff3, out_dir, int(threads))
--- bin/run_gmap.py
+++ bin/run_gmap.py
@@ -4,40 +4,39 @@ import os
 
 
 def run_gmap(ref_mono, in_cds, allele_cnt, workdir, threads):
-	ref_mono = os.path.abspath(ref_mono)
-	in_cds = os.path.abspath(in_cds)
+    ref_mono = os.path.abspath(ref_mono)
+    in_cds = os.path.abspath(in_cds)
 
-	if not os.path.exists(workdir):
-		os.mkdir(workdir)
-	print("Entering %s"%workdir)
-	os.chdir(workdir)
-	
-	if not os.path.exists("CpDB"):
-		cmd = "gmap_build -D . -d CpDB %s &> /dev/null"%ref_mono
-		print("Running command: %s"%cmd)
-		os.system(cmd)
-	else:
-		print("Gmap db found, skip")
-	
-	if not os.path.exists("gmap.gff3"):
-		fs = os.path.getsize(ref_mono)
-		if fs >= 2**32:
-			cmd = "gmapl -D . -d CpDB -f 2 -n %s -t %s %s 1> gmap.gff3 2>/dev/null"%(allele_cnt, threads, in_cds)
-		else:
-			cmd = "gmap -D . -d CpDB -f 2 -n %s -t %s %s 1> gmap.gff3 2>/dev/null"%(allele_cnt, threads, in_cds)
-		
-		
-		print("Running command: %s"%cmd)
-		os.system(cmd)
-	else:
-		print("gmap result found, skip")
-	
-	print("Finished")
+    if not os.path.exists(workdir):
+        os.mkdir(workdir)
+    print("Entering %s" % workdir)
+    os.chdir(workdir)
+
+    if not os.path.exists("CpDB"):
+        cmd = "gmap_build -D . -d CpDB %s &> gmap_build.log" % ref_mono
+        print("Running command: %s" % cmd)
+        os.system(cmd)
+    else:
+        print("Gmap db found, skip")
+
+    if not os.path.exists("gmap.gff3"):
+        fs = os.path.getsize(ref_mono)
+        if fs >= 2 ** 32:
+            cmd = "gmapl -D . -d CpDB -f 2 -n %s -t %s %s 1> gmap.gff3 2>gmap.err" % (allele_cnt, threads, in_cds)
+        else:
+            cmd = "gmap -D . -d CpDB -f 2 -n %s -t %s %s 1> gmap.gff3 2>gmap.err" % (allele_cnt, threads, in_cds)
+
+        print("Running command: %s" % cmd)
+        os.system(cmd)
+    else:
+        print("gmap result found, skip")
+
+    print("Finished")
 
 
 if __name__ == "__main__":
-	if len(sys.argv) < 6:
-		print("Usage: python %s <ref_mono> <in_cds> <allele_count> <workdir> <threads>"%sys.argv[0])
-	else:
-		ref_mono, in_cds, allele_cnt, workdir, threads = sys.argv[1:]
-		run_gmap(ref_mono, in_cds, allele_cnt, workdir, threads)
+    if len(sys.argv) < 6:
+        print("Usage: python %s <ref_mono> <in_cds> <allele_count> <workdir> <threads>" % sys.argv[0])
+    else:
+        ref_mono, in_cds, allele_cnt, workdir, threads = sys.argv[1:]
+        run_gmap(ref_mono, in_cds, allele_cnt, workdir, threads)
